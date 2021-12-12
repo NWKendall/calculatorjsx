5eedaeb9b4d4766f04f45e9177e6a11ac6091161
Nic Kendall <nic.kendall.dev@protonmail.com>
2021-12-12 17:29:16 -0400

    operations working

--- src/Reducer.js
+++ src/Reducer.js
@@ -6,23 +6,65 @@ export const ACTIONS = {
   EVALUATE: "evaluate",
 };
 
-
 export function Reducer(state, { type, payload }) {
-    switch (type) {
-      case ACTIONS.ADD_DIGIT:
-        if (payload.digit === "0" && state.currentOperand === "0") return state;
-        if (payload.digit === "." && state.currentOperand.includes("."))
-          return state;
+  switch (type) {
+    case ACTIONS.ADD_DIGIT:
+      if (payload.digit === "0" && state.currentOperand === "0") return state;
+      if (payload.digit === "." && state.currentOperand.includes("."))
+        return state;
+      return {
+        ...state,
+        currentOperand: `${state.currentOperand || ""}${payload.digit}`,
+      };
+
+    case ACTIONS.CLEAR:
+      return {};
+
+    case ACTIONS.CHOOSE_OPERATION:
+      if (state.currentOperand == null && state.previousOperand == null)
+        return state;
+      if (state.previousOperand == null) {
         return {
           ...state,
-          currentOperand: `${state.currentOperand || ""}${payload.digit}`,
+          operation: payload.operation,
+          previousOperand: state.currentOperand,
+          currentOperand: null,
         };
-  
-      case ACTIONS.CLEAR:
-        return {}
+      }
 
-      
-      default:
-        console.log("asdasda");
-    }
+      return {
+        ...state,
+        previousOperand: evaluate(state),
+        operation: payload.operation,
+        currentOperand: null,
+      };
+
+    default:
+      console.log();
   }
+}
+
+function evaluate({ currentOperand, previousOperand, operation }) {
+  const prev = parseFloat(previousOperand);
+  const curr = parseFloat(currentOperand);
+  if (isNaN(prev) || isNaN(curr)) return "";
+  let computation = "";
+  switch (operation) {
+    case "+":
+      computation = prev + curr;
+      break;
+
+    case "-":
+      computation = prev - curr;
+      break;
+
+    case "*":
+      computation = prev * curr;
+      break;
+
+    case "/":
+      computation = prev / curr;
+  }
+
+  return computation
+}
