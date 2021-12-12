3068826721d6fccdb76b5d11038fd72527330463
Nic Kendall <nic.kendall.dev@protonmail.com>
2021-12-12 16:25:06 -0400

    feat: calcualtor functionality

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
+import { useReducer } from "react";
+import DigitButton from "./DigitButton";
 import "./styles.css";
 
+export const ACTIONS = {
+  ADD_DIGIT: "add-digit",
+  CHOOSE_OPERATION: "choose-operation",
+  CLEAR: "clear",
+  DELETE_DIGIT: "delete-digit",
+  EVALUATION: "evaluation",
+};
+
+function reducer(state, { type, payload }) {
+  switch (type) {
+    case ACTIONS.ADD_DIGIT:
+      return {
+        ...state,
+        currentOperand: `${currentOperand || ""}${payload.digit}`,
+      };
+  }
+}
+
 function App() {
+  const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(
+    reducer,
+    {}
+  );
+
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand"></div>
-        <div className="current-operand"></div>
+        <div className="previous-operand">
+          {previousOperand} {operation}
+        </div>
+        <div className="current-operand">{currentOperand}</div>
       </div>
       <button className="span2">AC</button>
       <button>DEL</button>
       <button>/</button>
-      <button>1</button>
+      <DigitButton digit="1" dispatch={dispatch} />
       <button>2</button>
       <button>3</button>
+      <button>*</button>
       <button>4</button>
       <button>5</button>
       <button>6</button>
